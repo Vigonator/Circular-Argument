ff036eadd3aca11067fdc8489c0353b902cff279
Vigonator <the.vigonator@gmail.com>
2017-02-18 15:59:09 +0100

    Physics
    not working

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -6,12 +6,18 @@ public class playerMovement : MonoBehaviour {
 
     public GameObject world;
 
-    private float gravity = 9.81f;
+    public float gravity = 9.81f;
+
+    private Vector2 verticalVelocity;
 
     private bool falling = true;
 
-	// Use this for initialization
-	void Start () {
+    private const float wallAngle = 90.0f;
+
+    private Collision floor;
+    private Collision wall;
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
@@ -21,21 +27,98 @@ public class playerMovement : MonoBehaviour {
         if(falling == true)
         {
             fall();
+
+            
+
+            Debug.Log("onCollisionEnter");
         }
 
-        this.transform.LookAt(world.transform.position);
+        adjustAngle();
 
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        movement();
+    }
 
-        Vector3 movement = new Vector3(0.0f, moveHorizontal, 0.0f);
-        this.transform.RotateAround(world.transform.position,Vector3.back,moveHorizontal);
+    public void OnTriggerEnter(Collision collision)
+    {
+        Debug.Log("onCollisionEnter");
 
-        if(moveVertical >0 && falling==false)
-        { 
-        this.transform.Translate(0.0f, 0.0f, -moveVertical);
+        Vector3 colliderNormal = collision.contacts[0].normal;
+        Vector3 colliderPosition = collision.contacts[0].point;
+        Vector3 colliderWorldPosition = (colliderPosition - world.transform.position);
+
+        float contactAngle = Vector3.Angle(new Vector2(colliderNormal.x, colliderNormal.y), new Vector2(colliderNormal.x, colliderNormal.y));
+
+        if (contactAngle < 90)
+        {
+            floor = collision;
+            falling = false;
+            Debug.Log("floor");
+        }
+
+        else
+        {
+            wall = collision;
+            Debug.Log("wall");
+        }
+
+    }
+
+    private void OnTriggerExit(Collision collision)
+    {
+
+        Debug.Log("onCollisionExit");
+
+        if (collision.gameObject == floor.gameObject)
+        {
+            floor = null;
+            falling = true;
+            Debug.Log("not a floor anymore");
+        }
+
+        else if(collision.gameObject == wall.gameObject)
+        {
+            wall = null;
+            
         }
     }
+
+    void movement()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+
+        if(wall != null)
+        {
+            float playerWorldAngle = Vector3.Dot(this.gameObject.transform.position, Vector3.up);
+            float playerWallAngle = Vector3.Dot(wall.contacts[0].point, Vector3.up);
+
+            if(playerWorldAngle > playerWallAngle  )
+            {
+
+            }
+        }
+        else
+        {
+            this.gameObject.transform.RotateAround(world.transform.position, Vector3.forward, -horizontalMovement * 35 * Time.deltaTime);
+        }
+    }
+
+
+    void onTriggerStay(Collider other)
+    {
+        
+
+
+    }
+
+    void adjustAngle()
+    {
+        if(floor!=null)
+        { 
+        this.gameObject.transform.rotation = Quaternion.LookRotation(floor.contacts[0].point);
+        }
+    }
+
+
 
     void fall()
     {
@@ -43,28 +126,8 @@ public class playerMovement : MonoBehaviour {
         Vector3 playerPosition = this.transform.position;
         Vector3 direction = Vector3.Normalize(origin - playerPosition);
 
-        this.transform.Translate(direction * Time.deltaTime, Space.World);
+        this.transform.Translate(direction * gravity * Time.deltaTime, Space.World);
     }
 
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("World"))
-        {
 
-            falling = false;
-
-        }
-
-    }
-
-    void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.CompareTag("World"))
-        {
-
-            falling = true;
-
-        }
-
-    }
 }
